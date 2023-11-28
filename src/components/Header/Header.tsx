c88e337e9f3e9e09c0c7e90d83e8f6c308917085
Ayush Yadav <ayush.yadav@foundit.ai>
2023-11-28 10:00:14 +0530

    json placeholder api added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
+import React from 'react';
 
-const HEADER_LINK = [
-  {
-    name: 'Home',
-    url: '/'
-  },
-  {
-    name: 'About',
-    url: '/about'
-  },
-  {
-    name: 'Contact',
-    url: '/contact'
-  }
-]
+type Todo = {
+  "userId": number,
+  "id": number,
+  "title": string,
+  "completed": boolean
+}
 
 const Header = () => {
+  const [todosData, setTodosData] = React.useState<Todo[] | []>([])
+
+  React.useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((json) => setTodosData(json))
+  }, [])
+
   return (
     <div>
       <ul>
-        {HEADER_LINK.map((link, index) => (
-          <li key={index}>
-            <a href={link.url}>{link.name}</a>
+        {todosData && todosData.length > 0 ? todosData.map((todo, index) => (
+          <li key={index} className='boder-white p-2 rounded hover:shadow'>
+            <h3>{todo.title}</h3>
+            <p>Id: {todo.id}</p>
+            <p>User Id: {todo.userId}</p>
+            <p>{todo.completed ? 'Completed' : 'Not Completed'}</p>
           </li>
-        ))}
+        )) : <li>No data, please wait...</li>
+        }
       </ul>
     </div>
   )
